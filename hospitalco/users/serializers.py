3bb4e7fc48bb0430d71cce0d5e02212159b1e45e
akshay <akshaytalks@gmail.com>
2020-03-26 17:16:38 +0530

    Created setrializers for Hospital model
    Resolves #2

--- hospitalco/users/serializers.py
+++ hospitalco/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, HospitalProfile, VendorProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -20,6 +20,31 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'email', 'auth_token',)
+        fields = ('id', 'username', 'password', 'email','name','is_hospital','phoneNumber','address','auth_token',)
         read_only_fields = ('auth_token',)
         extra_kwargs = {'password': {'write_only': True}}
+
+
+class CreateHospitalSerializer(serializers.ModelSerializer):
+    user = CreateUserSerializer()
+    class Meta:
+        model = HospitalProfile
+        fields = ['user','hospitalId']
+    def create(self, validated_data):
+        userdata = validated_data.pop('user')
+        userInstance = User.objects.create_user(**userdata)
+        hospitalProfile = HospitalProfile.objects.create(user=userInstance,**validated_data)
+        return hospitalProfile
+
+class CreateVendorSerializer(serializers.ModelSerializer):
+    user = CreateUserSerializer()
+    class Meta:
+        model = VendorProfile
+        fields = ['user','gstin']
+    def create(self, validated_data):
+        userdata = validated_data.pop('user')
+        userInstance = User.objects.create_user(**userdata)
+        vendorProfile = VendorProfile.objects.create(user=userInstance,**validated_data)
+        return vendorProfile
+
+
--- hospitalco/users/views.py
+++ hospitalco/users/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
-from .models import User
+from .models import User, HospitalProfile, VendorProfile
 from .permissions import IsUserOrReadOnly
-from .serializers import CreateUserSerializer, UserSerializer
+from .serializers import CreateUserSerializer, CreateHospitalSerializer, CreateVendorSerializer, UserSerializer
 
 
-class UserViewSet(mixins.RetrieveModelMixin,
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin,
                   viewsets.GenericViewSet):
     """
@@ -16,6 +17,7 @@ class UserViewSet(mixins.RetrieveModelMixin,
     permission_classes = (IsUserOrReadOnly,)
 
 
+
 class UserCreateViewSet(mixins.CreateModelMixin,
                         viewsets.GenericViewSet):
     """
@@ -23,4 +25,22 @@ class UserCreateViewSet(mixins.CreateModelMixin,
     """
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
+    permission_classes = (AllowAny,)
+
+class HospitalCreateViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = HospitalProfile.objects.all()
+    serializer_class = CreateHospitalSerializer
+    permission_classes = (AllowAny,)
+
+class VendorCreateViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = VendorProfile.objects.all()
+    serializer_class = CreateVendorSerializer
     permission_classes = (AllowAny,)
