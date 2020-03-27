c9c4076a12b43962356303a653bd99248858f3b3
Tejas Arlimatti <tejasarlimatti@gmail.com>
2020-03-27 17:26:28 +0530

    CRUD APIs issue #10

--- hospitalco/users/views.py
+++ hospitalco/users/views.py
@@ -1,46 +1,157 @@
-from rest_framework import viewsets, mixins
-from rest_framework.permissions import AllowAny
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import viewsets, mixins, views
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import authenticate, SessionAuthentication, BasicAuthentication 
+from rest_framework.response import Response
 from .models import User, HospitalProfile, VendorProfile
 from .permissions import IsUserOrReadOnly
-from .serializers import CreateUserSerializer, CreateHospitalSerializer, CreateVendorSerializer, UserSerializer
+from .serializers import CreateUserSerializer, UserSerializer, HospitalProfileSerializer, \
+    VendorProfileSerializer, CreateHospitalSerializer, CreateVendorSerializer
 
 
-class UserViewSet(mixins.ListModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.UpdateModelMixin,
-                  viewsets.GenericViewSet):
-    """
-    Updates and retrieves user accounts
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsUserOrReadOnly,)
+class CsrfExemptSessionAuthentication(SessionAuthentication):
 
+    def enforce_csrf(self, request):
+        return  # To not perform the csrf check previously happening
 
-
-class UserCreateViewSet(mixins.CreateModelMixin,
+class UserRegisterViewSet(mixins.CreateModelMixin,
                         viewsets.GenericViewSet):
     """
-    Creates user accounts
+    Registers user accounts
     """
+
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = (AllowAny,)
 
-class HospitalCreateViewSet(mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+class UserDetailsView(views.APIView):
     """
-    Creates user accounts
+    Returns current authenticated user's information
     """
-    queryset = HospitalProfile.objects.all()
-    serializer_class = CreateHospitalSerializer
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def get(self, request):
+
+        user = request.user
+
+        return Response(UserSerializer(user).data)
+
+class UserLoginView(views.APIView):
+    """
+    Logs in a user
+    """
+
+    permission_classes = (AllowAny,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def post(self, request):
+        
+        data = request.data
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+class UserLogoutView(views.APIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def get(self, request):
+
+        logout(request)
+        
+        return Response(status=status.HTTP_200_OK)
+
+class UserListSet(mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    Updates and retrieves user accounts
+    """
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
-class VendorCreateViewSet(mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+class HospitalViewSet(mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
     """
-    Creates user accounts
+    Updates and retrieves user accounts
     """
-    queryset = VendorProfile.objects.all()
-    serializer_class = CreateVendorSerializer
+    
+    queryset = HospitalProfile.objects.all()
+    serializer_class = HospitalProfileSerializer
     permission_classes = (AllowAny,)
+
+class VendorViewSet(mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    Updates and retrieves user accounts
+    """
+    
+    queryset = VendorProfile.objects.all()
+    serializer_class = VendorProfileSerializer
+    permission_classes = (AllowAny,)
+
+class UserHospitalView(viewsets.ViewSet):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def get(self, request):
+
+        try:
+            queryset = HospitalProfile.objects.get(user=request.user)
+            serializer = HospitalProfileSerializer(queryset)
+            return Response(serializer.data)
+
+        except HospitalProfile.DoesNotExist:
+            return Response(status=status.HTTP_200_OK)
+
+    def post(self, request):
+
+        serializer = CreateHospitalSerializer(data=request.data, context={'user': request.user})
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+        serializer.save()
+
+        return Response(serializer.data)
+
+class UserVendorView(viewsets.ViewSet):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+
+    def get(self, request):
+
+        try:
+            queryset = VendorProfile.objects.get(user=request.user)
+            serializer = VendorProfileSerializer(queryset)
+            return Response(serializer.data)
+
+        except VendorProfile.DoesNotExist:
+            return Response(status=status.HTTP_200_OK)
+
+    def post(self, request):
+
+        serializer = CreateVendorSerializer(data=request.data, context={'user': request.user})
+        if not serializer.is_valid():
+            return Response(serializer.errors)
+        serializer.save()
+
+        return Response(serializer.data)
+        
