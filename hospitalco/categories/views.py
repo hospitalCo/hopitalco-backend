1a28b21306f7989597b02ebe40907287b8b8792f
Ananya Bahadur <ananya95@gmail.com>
2020-03-28 04:41:55 +0530

    feat: Adds basic CRUD for categories
    Reolves #12

---
+++ hospitalco/categories/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
